4274e86c7400ae87b013f837c4d57279bf2bb0df
ashok <ashokvishwakarma@nimapinfotech.com>
2023-06-09 18:30:20 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { HomeComponent } from './home/home.component';
+import { CONTACTComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatTableModule} from '@angular/material/table';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CONTACTComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatPaginatorModule,
+    MatBadgeModule,
+    MatTableModule
   ],
+
+
   providers: [],
   bootstrap: [AppComponent]
+
+
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
